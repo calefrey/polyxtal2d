a4a2577e5e409b4eb7b6ea74702bf7b2c30691c5
Caleb Frey <caleb.frey@rutgers.edu>
2021-08-05 12:10:11 -0400

    add animation to plot

--- R-curve/plotter.py
+++ R-curve/plotter.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 from sys import argv
-import addcopyfighandler  # so we can copy figure to clipboard
+import matplotlib.animation as ani
+
 
 # with open(argv[1], "r") as f:
 with open("r-curve.txt", "r") as f:
@@ -13,14 +14,23 @@ with open("r-curve.txt", "r") as f:
             x, y = line.split()
             x_arr.append(float(x))
             y_arr.append(float(y))
-            print(f"x={x}, y={y}")
 
-    plt.plot(x_arr, y_arr)
-    plt.ylabel("$K_I$")
-    plt.xlabel("$\\frac{a}{w}$")
-    axes = plt.gca()
+fig = plt.figure()
+plt.ylabel("$K_I$")
+plt.xlabel("$\\frac{a}{w}$")
+axes = plt.gca()
 
-    axes.xaxis.label.set_size(14)
+axes.xaxis.label.set_size(14)
 
-    axes.yaxis.label.set_size(14)
+axes.yaxis.label.set_size(14)
+
+
+def builddata(i):
+    p = plt.plot(x_arr[:i], y_arr[:i], marker=".", color="tab:blue")
+    print(f"x={x_arr[i]:.2E}, y={y_arr[i]:.2E}")
+    return p
+
+
+animator = ani.FuncAnimation(fig, builddata, frames=len(x_arr), repeat=False)
 plt.show()
+fig.savefig(f"r-curve.png")  # saves final r-curve to file
